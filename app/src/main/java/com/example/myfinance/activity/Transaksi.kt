978438482db79ec482ae7883982970ea8ae5841e
aldryarief <aldreva@gmail.com>
2020-12-19 15:59:58 +0700

    kategori done

--- app/src/main/java/com/example/myfinance/activity/Transaksi.kt
+++ app/src/main/java/com/example/myfinance/activity/Transaksi.kt
@@ -1,12 +1,24 @@
 package com.example.myfinance.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import com.example.myfinance.R
+import java.text.NumberFormat
+import java.util.*
+
 
 class Transaksi : AppCompatActivity() {
+    var jmlTrans : EditText? = null
+    var nilai : String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_transaksi)
+        jmlTrans = findViewById(R.id.jmltrans) as EditText
+
+
+
     }
 }
